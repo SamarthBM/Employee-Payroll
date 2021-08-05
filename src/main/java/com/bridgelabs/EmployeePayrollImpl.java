44676ec6330ceed6883563e763518100c9f9faac
SamarthBM <samarthbm18@gmail.com>
2021-08-05 13:40:57 +0530

    Initial commit

---
+++ src/main/java/com/bridgelabs/EmployeePayrollImpl.java
@@ -0,0 +1,16 @@
+package com.bridgelabs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollImpl {
+
+    private List<EmployeePayrollData> employeePayrollDatalist = new ArrayList<EmployeePayrollData>();
+
+    private void readData(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter employee ");
+    }
+}
